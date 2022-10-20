69693dd59cc37e30f8afcf51263396eb1a213481
MrNeibert <root@PC-WillianOficial-7538423.localdomain>
2022-10-20 20:43:06 -0300

    add .js and start linking the HTML

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const clearButton = document.querySelector('[data-clear]')
+const equalButton = document.querySelector('[data-equal]')
+const deleteButton = document.querySelectorAll('[data-delete]')
+const previousTextElement =document.querySelector('[data-previous]')
+const currentTextElement =document.querySelector('[data-current]')
